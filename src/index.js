ff1f69e8ab5e4940cae72178cd11f0a24077c398
BreElle Wells <breellejordyn@gmail.com>
2023-07-29 12:03:10 -0600

    render registered dog data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {
+const dogUrl = 'http://localhost:3000/dogs'
 
-})
+const registeredDogTable = document.getElementById('table-body')
+
+// fetch data and render registered dogs
+fetch(dogUrl)
+    .then(r => r.json())
+    .then(dogData => renderDogs(dogData))
+
+function renderDogs(allDogs) {
+    allDogs.forEach((singleDog) => {
+        const dogName = document.createElement('td')
+        dogName.innerText = singleDog.name
+
+        const dogBreed = document.createElement('td')
+        dogBreed.innerText = singleDog.breed
+
+        const dogSex = document.createElement('td')
+        dogSex.innerText = singleDog.sex
+
+        const dogEditBtn = document.createElement('button')
+        dogEditBtn.innerText = "Edit Dog"
+
+        const dogBtnCell = document.createElement('td')
+        dogBtnCell.appendChild(dogEditBtn)
+
+        const dogRow = document.createElement('tr')
+        dogRow.append(dogName, dogBreed, dogSex, dogBtnCell)
+
+        registeredDogTable.appendChild(dogRow)
+    })
+}
